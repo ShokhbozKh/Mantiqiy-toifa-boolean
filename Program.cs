f6474fe37a1895eaa99a2525fa901c9a12b55fd1
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-03 18:06:08 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Mantiqiy_toifa_boolean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isTrue = true;
+            bool isFalse = false;
+
+            Console.WriteLine("isTrue qiymati: " + isTrue);
+            Console.WriteLine("isFalse qiymati: " + isFalse);
+
+            // Mantiqiy operatorlar: && (va), || (yoki), ! (not)
+            bool resultAnd = isTrue && isFalse;
+            bool resultOr = isTrue || isFalse;
+            bool resultNot = !isTrue;
+
+            Console.WriteLine("Va (&&) operatori: " + resultAnd);
+            Console.WriteLine("Yoki (||) operatori: " + resultOr);
+            Console.WriteLine("Not (!) operatori: " + resultNot);
+
+            Console.WriteLine(Math.Sign(-423234));
+        }
+    }
+}
